26ab734897276a19adcbf46f645d1006b3cbbae1
Willem van der Jagt <wkjagt@gmail.com>
2021-04-22 08:50:25 -0400

    upload script

--- Makefile
+++ Makefile
@@ -4,7 +4,6 @@ assemble:
 upload_rom:
 	minipro -p AT28C256 -w bootloader.rom
 
-upload_program:
-	cd upload_script
-	bundle exec ruby serial.rb
-	cd ..
+load:
+	./vasm/vasm6502_oldstyle -Fbin -dotdir blink_led.asm -o blink.rom
+	ruby upload_script/serial.rb
--- upload_script/serial.rb
+++ upload_script/serial.rb
@@ -1,24 +1,51 @@
+require 'bundler/inline'
+
+gemfile do
+  source "https://rubygems.org"
+  git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+  
+  gem "serialport"
+  gem "pry"
+  gem "progress_bar"
+  gem "colorize"
+end
+
+ROM = 'blink.rom'
+
 require "serialport"
 require "pry"
-serial = SerialPort.new("/dev/tty.usbserial-A700fbj9", 19200, 8, 1, SerialPort::NONE)
+require 'progress_bar'
+require 'colorize'
 
+begin
+  serial = SerialPort.new("/dev/tty.usbserial-A700fbj9", 19200, 8, 1, SerialPort::NONE)
+rescue Errno::ENOENT
+  puts "\nSerial not plugged in\n\n"
+  exit
+end
 
-# smol porgram to wryte to lcd
+class ProgressBar
+  def render_rate
+    "[%#{max_width + 3}.2fB/s]" % rate
+  end
+end
 
+puts 
+puts ("Uploading " + ROM.colorize(:light_blue).bold + " to 6502 computer").underline
+puts
 
-payload = [
-  0x05, #0x00,         # length low byte, high byte
-  0xa9, 0x01,         # lda #1
-  0x8d, 0x01, 0x60,   # sta $6001
-]
-
-file = File.open('../blink.rom') do |file|
+file = File.open(ROM) do |file|
   bytes = file.read.bytes
   size_bytes = [bytes.size].pack('S').unpack('CC')
-  
+  payload = [size_bytes.first] + bytes
+
+  bar = ProgressBar.new(payload.size)
+
   serial.putc("l")
-  ([size_bytes.first] + bytes).each do |byte|
-    sleep(0.01)
+
+  payload.each do |byte|
+    sleep(0.05)
     serial.putc(byte)
+    bar.increment!
   end
 end
