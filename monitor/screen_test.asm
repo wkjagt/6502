32a65d26379a4e7b86dea1a248773b31ac7cdc6a
Willem van der Jagt <wkjagt@gmail.com>
2021-11-05 11:39:08 -0400

    screen is pretty functional

--- monitor/screen_test.asm
+++ monitor/screen_test.asm
@@ -6,31 +6,42 @@ IO_PCR   = $600c                        ; Peripheral control register
 IO_IFR   = $600d                        ; Interrupt flag register
 IO_IER   = $600e                        ; Interrupt enable register
 
-; pins:
-; 2 : 0000.0100 AVAIL (write)
-; 3 : 0000.1000 ACK   (read)
-; 4 : 0001.0000 DATA0 (read)
-; 5 : 0010.0000 DATA1 (read)
-; 6 : 0100.0000 DATA2 (read)
-; 7 : 1000.0000 DATA3 (read)
 
-; char A: ASCII: 0100.0001
+CLEAR_SCREEN    =       $0c
+CHOOSE_CURSOR   =       2
+CURSOR_CHAR     =       $db
+CURSOR_BLINK    =       3
+
+DATA_PINS       =       %11110000
+AVAILABLE       =       %00000100
+ACK             =       %00001000
+OUTPUT_PINS     =       DATA_PINS | AVAILABLE
+UNUSED_PINS     =       %00000011
+
                 .ORG    $0700
 
 screen_init:
                 ; set data direction for the 6 needed pins, while keeping
                 ; the unused ones unchanged
                 lda     IO_DDRA
-                ora     #%11110100      ; set output bits (1s)
-                and     #%11110111      ; set input bits (0s)
+                ora     #OUTPUT_PINS
+                and     #(OUTPUT_PINS | UNUSED_PINS)
                 sta     IO_DDRA
 
                 ; set all used pins low, while keeping the unused ones unchanged
                 lda     IO_PORTA
-                and     #%00000011
+                and     #UNUSED_PINS
                 sta     IO_PORTA
 
-
+                lda     #CLEAR_SCREEN
+                jsr     send_byte
+                lda     #CHOOSE_CURSOR
+                jsr     send_byte
+                lda     #CURSOR_CHAR
+                jsr     send_byte
+                lda     #CURSOR_BLINK
+                jsr     send_byte
+                
                 ldx     #0
 .text_loop:
                 lda     text,x
@@ -42,17 +53,23 @@ screen_init:
                 rts
 
 text:                             ; CR   LF  Null
-    .asciiz "123", $00
+    .asciiz "Shallow Thought v0.01 / 14-10-2021", $0d, $0a, $00
 
 send_byte:
-                pha                     ; we need this back later for the low nibble
+                pha                     ; we pull off the arg twice, once for high
+                pha                     ; nibble and once for low nibble
+
+                lda     IO_PORTA
+                and     #!DATA_PINS     ; clear data
+                sta     IO_PORTA
+
                 jsr     wait_ack_low
-                
+                pla
                 and     #%11110000      ; mask out low nibble
                 ora     IO_PORTA
                 sta     IO_PORTA
 
-                ora     #%00000100      ; flip available = high
+                ora     #AVAILABLE      ; flip available = high
                 sta     IO_PORTA
 
                 jsr     wait_ack_high
@@ -61,15 +78,15 @@ send_byte:
                 sta     IO_PORTA
 
                 pla                     ; get the original byte back
-                asl                     ; shift low nibble i
+                asl                     ; shift low nibble into high nibble
                 asl                     
                 asl
                 asl                     
 
-                ora     IO_PORTA        ; 0001 0100
+                ora     IO_PORTA
                 sta     IO_PORTA
 
-                and     #%11111011          ; 0001 0000
+                and     #~AVAILABLE     ; flip available = low
                 sta     IO_PORTA
 
                 jsr     wait_ack_low
@@ -81,7 +98,7 @@ wait_ack_high:
                 pha
 .loop
                 lda     IO_PORTA
-                and     #%00001000
+                and     #ACK
                 beq     .loop
                 pla
                 rts
@@ -89,11 +106,7 @@ wait_ack_low:
                 pha
 .loop:
                 lda     IO_PORTA
-                and     #%00001000
+                and     #ACK
                 bne     .loop
                 pla
                 rts
-
-
-
-
