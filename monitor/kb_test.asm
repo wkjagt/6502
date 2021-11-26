259507378e0fc76efdd7454699c1349ac3c52b0f
Willem van der Jagt <wkjagt@gmail.com>
2021-11-26 08:57:47 -0500

    testing the keyboard controller

--- monitor/kb_test.asm
+++ monitor/kb_test.asm
@@ -3,9 +3,10 @@ SER_ST          =       $4801           ; Status register
 SER_CMD         =       $4802           ; Command register
 SER_CTL         =       $4803           ; Control register
 SER_RXFL        =       %00001000       ; Serial Receive full bit
+PORTB           =       $6000           ; Data port B
 PORTA           =       $6001           ; Data port A
+PORTB_DDR       =       $6002           ; Data direction of port B
 PORTA_DDR       =       $6003           ; Data direction of port A
-
 
 CHAR            =       $0
 ACK             =       %01000000
@@ -14,58 +15,51 @@ ACK             =       %01000000
 
 setup:
                 ; 0 = input, 1 = output
-                lda     #%01000000
-                sta     PORTA_DDR
-                ldx     #0
-.startup_text_loop:
-                lda     startup_text, x
-                beq     .done
-                jsr     write_to_terminal
-                inx
-                jmp     .startup_text_loop
-.done:
-                jsr     keyboard
-                rts
+                lda     #ACK
+                sta     PORTB_DDR
 
-keyboard:
+keyboard_loop:
+                ; receive the character
                 jsr     receive_nibble
                 jsr     receive_nibble
 
+                ; write the character
                 lda     CHAR
                 jsr     write_to_terminal
 
-                jmp     keyboard
+                ; receive the flags
+                jsr     receive_nibble
+
+                jmp     keyboard_loop
 
 receive_nibble:
-                lda     PORTA
-                bpl     receive_nibble
-
-                asl                     ; move low nibble to high nibble
-                asl
-                asl
-                asl
+                lda     PORTB           ; LDA loads bit 7 (avail) into N
+                bpl     receive_nibble  ; repeat until avail is 1
 
                 ldx     #4
 .shift:
-                asl                     ; shift bit into carry
-                rol     CHAR            ; rotate carry into CHAR
+                asl                     ; move low nibble to high nibble
                 dex
                 bne     .shift
 
-                lda     PORTA           ; send ack signal to kb controller
+                ldx     #4
+.rotate:
+                asl                     ; shift bit into carry
+                rol     CHAR            ; rotate carry into CHAR
+                dex
+                bne     .rotate
+
+                lda     PORTB           ; send ack signal to kb controller
                 ora     #ACK
-                sta     PORTA
+                sta     PORTB
 .wait_avail_low:
-                lda     PORTA           ; wait for available to go low
+                lda     PORTB           ; wait for available to go low
                 bmi     .wait_avail_low ; negative means bit 7 (avail) high
 
-                lda     PORTA           ; set ack low
+                lda     PORTB           ; set ack low
                 and     #!ACK
-                sta     PORTA
+                sta     PORTB
                 rts
-
-
-
 
 ; Write to terminal (for debugging)
 write_to_terminal:
