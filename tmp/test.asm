7201ef4359c832dce3fb51af80bb63bbc44330c7
Willem van der Jagt <wkjagt@gmail.com>
2022-01-16 20:01:04 -0500

    dump command works

--- tmp/test.asm
+++ tmp/test.asm
@@ -2,6 +2,10 @@
 
 command_vector  =       $00
 keybuffer_ptr   =       $02
+tmp1            =       $04
+tmp2            =       $06
+dump_start      =       $08
+param_index     =       $0a
 keybuffer       =       $80
 
 SPACE           =       $20
@@ -10,7 +14,7 @@ CR              =       $0D
 DEL             =       $7F
 BS              =       $08
 
-                .org $8000:
+                .org $8000
 
 start:
                 ldx #0
@@ -77,16 +81,15 @@ next_key:
 execute_command:
                 ldx #0                  ; index into list of commands
 find_command_loop:
-                ; set up the pointer
                 lda commands,x
-                sta command_vector
+                sta tmp1
                 inx
                 lda commands,x
-                sta command_vector+1
+                sta tmp1+1
 
-                lda (command_vector)
+                lda (tmp1)
                 bne .continue
-                lda (command_vector+1)
+                lda (tmp1+1)
                 beq .done
 
 .continue:
@@ -101,7 +104,7 @@ match_command:
 .char_cmp_loop:
                 ; compare one character
                 lda keybuffer,y
-                cmp (command_vector),y
+                cmp (tmp1),y
                 bne .done
 .char_match
                 ; is it the last character?
@@ -111,27 +114,28 @@ match_command:
                 jmp .char_cmp_loop
 .string_match:         
                 iny     ; skip past the 0 at the end of the string
-
-                ; command_vector now points to the command that holds the address
-                ; to jump to. Store that address in command_vector directly so we
+                ; tmp1 now points to the command that holds the address
+                ; to jump to. Store that address in command_vector so we
                 ; can jump to it.
-                lda (command_vector), y
-                sta command_vector
+                sty param_index
 
-                lda (command_vector+1), y
-                lda command_vector+1
+                lda (tmp1), y
+                sta command_vector      
+                iny
+                lda (tmp1), y
+                sta command_vector+1
 
                 jmp (command_vector)
 .done
                 rts
 
-rcv:
-                lda keybuffer_ptr
+dump:
+                clc
+                lda #keybuffer
+                adc param_index
                 
-                ; inx
-                ; inx
-                ; lda keybuffer,x
-                jsr putnr
+                jsr hex_to_byte
+                jsr dump_page
                 rts
 
 command2:
@@ -155,11 +159,109 @@ getc:
                 beq getc
                 rts
 
-commands:
-                .word o_rcv, o_command2, o_command3, 0
 
-o_rcv:           .byte "rcv", 0
-                 .word rcv
+hex_to_byte:
+                sta     tmp2            ; we need the address to do lda (tmp2), y
+
+                ldy     #0              ; high byte
+                lda     (tmp2),y
+                jsr     shift_in_nibble
+
+                iny
+                lda     (tmp2),y        ; low byte
+                jsr     shift_in_nibble
+
+                lda     tmp1            ; put the result back in A as return value
+                rts
+shift_in_nibble:
+                cmp     #":"            ; the next ascii char after "9"
+                bcc     .number
+                                        ; assume it's a letter
+                sbc     #87             ; get the letter value
+                jmp     .continue
+.number:
+                sec
+                sbc     #48
+.continue:      
+                ; calculated nibble is now in low nibble
+                ; shift low nibble to high nibble
+                asl 
+                asl 
+                asl 
+                asl
+
+                ; left shift hight nibble into result
+                asl
+                rol     tmp1
+                asl
+                rol     tmp1
+                asl
+                rol     tmp1
+                asl
+                rol     tmp1
+
+                rts
+
+dump_page:
+                sta     dump_start+1
+                ldx     #0
+                ldy     #0
+.rowloop:
+                lda     (dump_start),y
+                jsr     print_byte_as_hex
+                lda     #" "
+                jsr     putc
+                iny
+                beq     .done
+                inx
+                cpx     #16
+                beq     .next_row
+                bra     .rowloop
+.next_row:
+                lda     #LF
+                jsr     putc
+                lda     #CR
+                jsr     putc
+                ldx     #0
+                bra     .rowloop
+.done:
+                rts
+
+
+
+print_byte_as_hex:
+                pha                     ; keep a copy for the low nibble
+
+                lsr                     ; shift high nibble into low nibble
+                lsr
+                lsr
+                lsr
+
+                jsr     print_nibble
+
+                pla                     ; get original value back
+                and     #%00001111      ; reset high nibble
+                jsr     print_nibble
+                rts
+
+print_nibble:
+                cmp     #10
+                bcs     .letter         ; >= 10 (hex letter A-F)
+                adc     #48             ; ASCII offset to numbers 0-9
+                jmp     .print
+.letter:
+                adc     #54             ; ASCII offset to letters A-F
+.print:
+                jsr     putc
+                rts
+
+
+                .org $a000
+commands:
+                .word o_dump, o_command2, o_command3, 0
+
+o_dump:          .byte "dump", 0
+                 .word dump
 o_command2:      .byte "command2", 0
                  .word command2
 o_command3:      .byte "command3", 0
