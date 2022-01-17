61027db976099630c8049b98c31cc812c62cdd41
Willem van der Jagt <wkjagt@gmail.com>
2022-01-17 08:28:55 -0500

    ascii in dump

--- tmp/test.asm
+++ tmp/test.asm
@@ -91,7 +91,6 @@ find_command_loop:
                 bne .continue
                 lda (tmp1+1)
                 beq .done
-
 .continue:
                 jsr match_command
                 inx
@@ -114,11 +113,11 @@ match_command:
                 jmp .char_cmp_loop
 .string_match:         
                 iny     ; skip past the 0 at the end of the string
+                sty param_index
+
                 ; tmp1 now points to the command that holds the address
                 ; to jump to. Store that address in command_vector so we
                 ; can jump to it.
-                sty param_index
-
                 lda (tmp1), y
                 sta command_vector      
                 iny
@@ -132,7 +131,7 @@ match_command:
 dump:
                 clc
                 lda #keybuffer
-                adc param_index
+                adc param_index         ; calculate the start of the param
                 
                 jsr hex_to_byte
                 jsr dump_page
@@ -206,24 +205,71 @@ dump_page:
                 sta     dump_start+1
                 ldx     #0
                 ldy     #0
-.rowloop:
-                lda     (dump_start),y
-                jsr     print_byte_as_hex
-                lda     #" "
-                jsr     putc
-                iny
-                beq     .done
-                inx
-                cpx     #16
-                beq     .next_row
-                bra     .rowloop
+
+; start of line (new line + start address)
+; x counts up to 16 for each row
+; y counts up to 256 for the whole page of memory
 .next_row:
                 lda     #LF
                 jsr     putc
                 lda     #CR
                 jsr     putc
+                
+                lda     dump_start+1
+                jsr     print_byte_as_hex
+                tya
+                jsr     print_byte_as_hex
+                lda     #" "
+                jsr     putc
+                lda     #" "
+                jsr     putc
+
                 ldx     #0
-                bra     .rowloop
+; raw bytes
+.next_hex_byte:
+                lda     (dump_start),y
+                jsr     print_byte_as_hex
+                lda     #" "
+                jsr     putc
+                iny
+                inx
+                cpx     #16
+                beq     .ascii
+                cpx     #8
+                bne     .next_hex_byte
+                lda     #" "
+                jsr     putc
+                bra     .next_hex_byte
+; ascii representation
+.ascii
+                ldx     #0
+                tya
+                sec
+                sbc     #16             ; rewind 16 bytes for ascii
+                tay
+                lda     #" "
+                jsr     putc
+                lda     #" "
+                jsr     putc
+.next_ascii_byte:
+                ; ascii: $20-$7E
+                lda     (dump_start),y
+                cmp     #$20            ; space
+                bcc     .not_ascii
+                cmp     #$7F
+                bcs     .not_ascii
+                jsr     putc
+                bra     .continue_ascii_byte
+.not_ascii:
+                lda     #"."
+                jsr     putc
+.continue_ascii_byte:
+                iny
+                beq     .done
+                inx
+                cpx     #16
+                beq     .next_row
+                bra     .next_ascii_byte
 .done:
                 rts
 
